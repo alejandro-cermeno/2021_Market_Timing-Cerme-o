96cdccfa61c1ddab1ec07c7915e37dbb27fc4962
alejandro-cermeno <77359430+alejandro-cermeno@users.noreply.github.com>
2021-11-01 15:34:13 -0500

    Replicates table 2

--- code/modelingVol_params.py
+++ code/modelingVol_params.py
@@ -1,21 +1,11 @@
 '''
-modelingVol_params.py
+This code generates and exports the Table 2: Estimated Parameters for daily Latin
+American Stock and Forex Markets Return (modelingVol_params.xlsx). In addition, 
+the summary of results for each model is saved in modelingVol_summary.txt. 
+Requires time, numpy, pandas, datetime, itertools and arch.
 
-Alejandro Cermeño (09/2021)
-
-Este codigo genera y exporta la tabla de coeficientes estimados
-(modelingVol_params.xlsx y modelingVol_params.tex) para los modelos y series 
-especificados. La diferencia entre ambos archivos está en la denominacion de la
-significancia en letras (a,b,c) o en asteriscos (***,**,*). Además, se 
-guarda el resumen de resultados de cada modelo en modelingVol_summary.txt.
-Se requiere los paquetes time, numpy, pandas, datetime, itertools y arch.
-
-Se omiten las tildes. Ver README.txt para informacion adicional.
+See README.txt for additional information.
 '''
-
-#############
-# Librerias #
-#############
 
 import time
 import numpy  as np
@@ -26,46 +16,35 @@ from itertools import product
 from arch      import arch_model
 from arch.__future__ import reindexing
 
-start = time.time()  # Inicio cronometro 
+start = time.time()  # stopwatch start 
 
 
 #############
-# Funciones #
+# Functions #
 #############
 
 def price2ret(price):
-  
   ret = (100 * ( np.log(price) - np.log(price.shift(1))))
-  
   return ret
 
+def export(df, file_name, excel = None, latex = None):
 
-def export(df, file_name, Excel = None, LaTeX = None):
-
-  # Exportar a Excel
-
-  if Excel == True:
-
+  # To Excel
+  if excel == True:
     df.to_excel(file_name + '.xlsx')  
-
-  # Exportar a LaTeX
-
-  if LaTeX == True:
+    
+  # To LaTeX
+  if latex == True:
     latex_code = df.to_latex()
-
     with open(file_name + '.tex', 'w') as tex:
       tex.write(latex_code)
 
-
 def name2disp(especificacion):
 
-  ''' 
-  Para cada especificación de la media, varianza o distribucion, establece
-  el nombre de la especificación a mostrarse.
-  '''
+    """For each specification of the mean, variance, or distribution, set 
+    the name of the specification to be displayed."""
 
-  # Nomnbre especificacion de la media
-
+  # Mean specification
   if especificacion == 'Zero':
     name2disp_ = 'Cero'
   elif especificacion == 'Constant':
@@ -73,25 +52,23 @@ def name2disp(especificacion):
   elif especificacion == 'AR':
     name2disp_ = 'AR'
 
-  # Nombre modelo de la varianza
-
+  # Variance model
   elif especificacion == arch_params:
     name2disp_ = 'ARCH'
   elif especificacion == garch_params:
     name2disp_ = 'GARCH'
   elif especificacion == grj_params:
     name2disp_ = 'GJR'
-  #elif especificacion == tarch_params:
+  #elif especificacion == tarch_params: # Not included
   #  name2disp_ = 'TARCH'
-  elif especificacion == egarch_params:
-    name2disp_ = 'EGARCH'
+  #elif especificacion == egarch_params: # Not included
+  #  name2disp_ = 'EGARCH'
   elif especificacion == aparch_params:
     name2disp_ = 'APARCH'
   elif especificacion == figarch_params:
     name2disp_ = 'FIGARCH'
 
-  # Nombre de la distribucion
-
+  # Error distributions
   elif especificacion == 'normal':
     name2disp_ = 'N'
   elif especificacion == 't':
@@ -102,28 +79,29 @@ def name2disp(especificacion):
     name2disp_ = 'GED'
 
   else:
-    name2disp_ = 'Especificacion no valida'
+    name2disp_ = 'Invalid specification'
 
   return name2disp_
 
 
 def signif_ast(coef, pvalues):
+    """Sets the confidence level in asterisks."""
  
   signif_ast = list()
 
   for p in range(len(pvalues)):
     if pvalues[p] <= 0.01:
-      asterisco = '***' # significativo al 1%                
+      asterisks = '***' # significant at 1%             
     elif pvalues[p] <= 0.05:                
-      asterisco = '**'  # significativo al 5%                   
+      asterisks = '**'  # significant at 5%                   
     elif pvalues[p] <= 0.1:
-      asterisco = '*'   # significativo al 10%                    
+      asterisks = '*'   # significant at 10%                    
     else:
-      asterisco = ''    # no significativo     
+      asterisks = ''    # not significant     
 
-    valor = str(coef[p]) + asterisco # Se une el coeficiente y el asterisco
+    value = str(coef[p]) + asterisks # joins the coefficient and the asterisks
 
-    signif_ast.append(valor)
+    signif_ast.append(value)
 
   signif_ast = pd.DataFrame(signif_ast, index = coef.index)
   
@@ -131,22 +109,23 @@ def signif_ast(coef, pvalues):
 
 
 def signif_letter(coef, pvalues):
+    """Sets the confidence level in asterisks."""
 
   signif_letter = list()
 
   for p in range(len(pvalues)):
     if pvalues[p] <= 0.01:
-      asterisco = '$^{a}$' # significativo al 1%                
+      asterisks = '$^{a}$' # significant at 1%                
     elif pvalues[p] <= 0.05:                
-      asterisco = '$^{b}$'  # significativo al 5%                   
+      asterisks = '$^{b}$'  # significant at 5%                   
     elif pvalues[p] <= 0.1:
-      asterisco = '$^{c}$'   # significativo al 10%                    
+      asterisks = '$^{c}$'   # significant at 10%                
     else:
-      asterisco = ''    # no significativo     
+      asterisks = ''    # not significant       
 
-    valor = str(coef[p]) + asterisco # Se une el coeficiente y el asterisco
+    value = str(coef[p]) + asterisks # joins the coefficient and the asterisks
 
-    signif_letter.append(valor)
+    signif_letter.append(value)
   
   signif_letter = pd.DataFrame(signif_letter, index = coef.index)
 
@@ -172,21 +151,19 @@ def model_info_fun(ts, mean, variance, dist):
   return info
 
 
-######################
-# Obtencion de datos #
-######################
+###################
+# Data collection #
+###################
 
-path = 'https://git.io/J6hzs'
+path = 'https://git.io/JuGLW'
 
-# Series del mercado bursatil
-
+# Stock market data
 stocks  = pd.read_excel(path, sheet_name = 'stocks_raw', index_col = 0)
 r_stocks = price2ret(stocks)
 
-# Series del mercado cambiario
-  
+# Forex market data  
 forex  = pd.read_excel(path, sheet_name = 'forex_raw', index_col = 0)
-r_forex = price2ret(forex)
+r_forex = 100 * (np.log(forex) - np.log(forex.shift(1)))
 
 returns = r_forex.join(r_stocks)
 
@@ -204,8 +181,8 @@ mean_ops       = ['Zero', 'Constant', 'AR']
 arch_params    = {'vol':'ARCH'}                               # ARCH
 garch_params   = {'p':1, 'q':1, 'vol':'GARCH'}                # GARCH  
 grj_params     = {'p':1, 'o':1, 'q':1, 'vol':'GARCH'}         # GRJ
-#tarch_params   = {'p':1, 'o':1, 'q':1, 'power':1.0}          # TARCH or ZARCH
-#egarch_params  = {'p':1, 'q':1, 'o':1, 'vol': 'EGARCH'}      # EGARCH
+#tarch_params   = {'p':1, 'o':1, 'q':1, 'power':1.0}          # TARCH  (not included)
+#egarch_params  = {'p':1, 'q':1, 'o':1, 'vol': 'EGARCH'}      # EGARCH (not included)
 aparch_params  = {'p':1, 'o':1, 'q':1, 'vol':'APARCH'}        # APARCH
 figarch_params = {'p':1, 'q':1, 'power':2.0, 'vol':'FIGARCH'} # FIGARCH
 
@@ -222,81 +199,79 @@ dist_ops     = ['normal', 't', 'skewt', 'ged']
 modelingVol_params_excel = pd.DataFrame()
 modelingVol_params_latex = pd.DataFrame()
 
-series_n = returns.shape[1] # Numero de series
+series_n = returns.shape[1] # number of series
 
-#######################################
-# Resultados para cada serie y modelo #
-#######################################
+#####################################
+# Results for each series and model #
+#####################################
 
 for market, mean, variance, dist in product(returns.columns, mean_ops, variance_ops, dist_ops):
 
     ts = returns[market].dropna()
 
-    # Se verifica si el modelo especificado tiene solucion
+    # It is verified if the specified model has a solution
     try:
 
-        # Especificacion y estimacion del modelo
+        # Specification and estimation of the model
         mdl = arch_model_v2(ts, mean = mean, **variance, dist = dist, 
                             rescale = False).fit(disp='off')
 
+        # Results if the model has a solution
+        
         # Save summary models results
-
         with open("modelingVol_summary.txt", "a+") as file_object:
           file_object.write(str(mdl))
           file_object.write("\n")
           file_object.write("\n")
-
-
-        # Resultados si el modelo tiene solucion
         
-        # Media, varianza y distribución para mostrar en la tabla
+        # Mean, variance and distribution to display in the table
         model_info = model_info_fun(ts, mean, variance, dist)
 
         # P-values
         pvalues = mdl.pvalues
 
-        # Coeficientes
+        # Coefficients
         coef = mdl.params.map('{:,.4f}'.format) # set 4 decimals precision
 
         coef_letter = pd.DataFrame(signif_letter(coef, pvalues))
         coef_ast = pd.DataFrame(signif_ast(coef, pvalues))
 
-        # Log-verosimilutud
+        # Log-like
         loglik = pd.Series(mdl.loglikelihood, index = ['log-lik'])
 
-        # Valor del criterio de informacion
+        # Information criterion value
         bic = pd.Series(mdl.bic, index = ['BIC'])
 
-        # Singular Matrix (0 = el modelo pudo ser estimado)
+        # Singular Matrix (0 = the model could be estimated)
         singular_matrix = pd.Series('0', index = ['Singular Matrix'])
 
-        #  Tablas resultado 
+        #  Result tables 
 
-        # Tabla para LaTeX (significancia con letras)
+        # Table for LaTeX (significancia con letras)
         #to_latex = pd.concat([model_info, coef_letter, bic, loglik, singular_matrix]) 
               
-        # Tabla para Excel (significancia con asteriscos)
+        # Table for Excel (significancia con asteriscos)
         to_excel = pd.concat([model_info,coef_ast, bic, loglik, singular_matrix]) 
 
 
-    # En caso no tener solucion:
-    
+    # In case there is no solution:
+  
     except np.linalg.LinAlgError:
         
-        # Media, varianza y distribución para mostrar en la tabla
+        # Mean, variance and distribution to display in the table
         model_info = model_info_fun(ts, mean, variance, dist)
         
-        # Singular Matrix (1 = el modelo no tuvo solución)
+        # Singular Matrix (1 = the model had no solution)
         singular_matrix = pd.Series('1', index = ['Singular Matrix'])
 
-        # Valores para las otras columnas (parametros, BIC, etc) en blanco
+        # Values for the other columns (parameters, BIC, etc) blank
         
-        #  Tablas resultado 
+        #  Results table
         #to_latex = pd.concat([model_info, singular_matrix])
         to_excel = pd.concat([model_info, singular_matrix])
         
         
-    # Se anexa la fila a las tablas de resultados (append)
+    # The row is appended to the results tables (append)
       
     to_excel = pd.DataFrame(to_excel).T
     #to_latex = pd.DataFrame(to_latex).T
@@ -310,11 +285,11 @@ for market, mean, variance, dist in product(returns.columns, mean_ops, variance_
     #                                                           ignore_index = True)
 
 
-##########################################
-# Organizacion de la tabla de resultados #
-##########################################
+####################################
+#Organization of the results table #
+####################################
 
-# Remplazar NaN por espacios en blanco
+# Replace NaN with blanks
 
 modelingVol_params_excel = modelingVol_params_excel.fillna('')
 #modelingVol_params_latex = modelingVol_params_latex.fillna('')
@@ -336,19 +311,17 @@ modelingVol_params_excel.rename(columns = {'alpha[1]':'alpha',
                                            'beta[1]':'beta',
                                            'gamma[1]':'gamma'},
                                 inplace = True)
-
 # Columns order
 
 col_order = ['serie', 'mean', 'variance', 'dist', 'mu', 'omega', 'alpha',
              'beta', 'gamma', 'delta', 'phi', 'nu', 'lambda', 'BIC', 'log-lik', 
-             'Singular Matrix']
-              
+             'Singular Matrix']     
 modelingVol_params_excel = modelingVol_params_excel[col_order]
 
 
-#############################
-# Exportacion de resultados #
-#############################
+#####################
+# Export of results #
+#####################
 
   # A Excel
 export(modelingVol_params_excel, 'modelingVol_params', Excel = True)
@@ -357,10 +330,10 @@ export(modelingVol_params_excel, 'modelingVol_params', Excel = True)
 #export(modelingVol_params_latex, 'modelingVol_params', LaTeX = True)
 
 
-# Se finaliza el codigo 
+# The code is finalized
 
-end = time.time()                                       # Fin cronometro
-time_exe = str(timedelta(seconds = round(end - start))) # Tiempo de ejecucion
+end = time.time()                                       # End of timer
+time_exe = str(timedelta(seconds = round(end - start))) # Execution time
 
-print('Ejecucion finalizada')
-print('Tiempo de ejecución: ', time_exe)
+print('Execution completed')
+print('Execution time: ', time_exe)
