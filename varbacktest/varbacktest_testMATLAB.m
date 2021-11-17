d9572556b353cad7cce868179a78f23d71250c6c
alejandro-cermeno <77359430+alejandro-cermeno@users.noreply.github.com>
2021-11-17 18:17:34 -0500

    Update varbacktest_testMATLAB.m

--- varbacktest/varbacktest_testMATLAB.m
+++ varbacktest/varbacktest_testMATLAB.m
@@ -15,6 +15,7 @@ VaR_ops = ["VaR_1", "VaR_5"];
 conf_lvl_ops = [0.99, 0.95];
 returns = df.mean_true;
 
+% backtest
 for i = 1 : length(VaR_ops)
     VaR = df.(i + 1);
     vbt = varbacktest(returns, VaR, 'VaRLevel', conf_lvl_ops(i));
@@ -29,20 +30,41 @@ for i = 1 : length(VaR_ops)
     PVcci = cci(vbt).PValueCCI;
     LRcc = cc(vbt).LRatioCC;
     PVcc = cc(vbt).PValueCC;
+    [DQ, PVdq] = dq(returns, VaR, conf_lvl_ops(i));
     
     results = table(conf_lvl_ops(i), obs, num_hits, pct_fails, TL, LRuc, PVuc, LRcci, ...
-                    PVcci, LRcc, PVcc, 'VariableNames', {'VaR_lvl', ...
+                    PVcci, LRcc, PVcc, DQ, PVdq, 'VariableNames', {'VaR_lvl', ...
                     'obs', 'num_hits', 'pct_fails', 'TL', 'LRuc', 'PVuc', 'LRcci', 'PVcci', ...
-                    'LRcc', 'PVcc'});
+                    'LRcc', 'PVcc', 'DQ', 'PVdq'});
     disp(results)
 end
 
-%   VaR_lvl    obs     num_hits        pct_fails         TL           LRuc                  PVuc                  LRcci                 PVcci                  LRcc                  PVcc         
-%   _______    ____    ________    __________________    ___    ________________    _____________________    ________________    ____________________    ________________    _____________________
+
+function [DQ, PVdq] = dq(returns, VaR, sigVaR)
+% Dynamic quantile test (DQ) of Engle and Manganelli (2004)
+    L = 4;
+    T = size(returns,1);
+    Hit = (returns < VaR) - sigVaR;
+    yy = Hit(L+1:T);
+    Z = nan(T-L, L+2);
+    for t = 1:T-L
+        Z(t,:) = [1 Hit(L+t-1:-1:t)' VaR(L+t-1)];
+    end
+    
+    deltahat = Z\yy;
+    DQ      = deltahat'*(Z'*Z)*deltahat/(sigVaR*(1-sigVaR));
+    PVdq    = 1 - chi2cdf(DQ, L+2);
+end
+
+% Output
 %
-%    0.99      1703      166       0.0974750440399295    red    471.596309196609    1.44012903404047e-104    294.473896975476    5.26938462847135e-66    766.070206172084    4.46651045074517e-167
+% VaR_lvl    obs     num_hits        pct_fails         TL           LRuc                  PVuc                  LRcci                 PVcci                  LRcc                  PVcc                    DQ           PVdq
+%  _______    ____    ________    __________________    ___    ________________    _____________________    ________________    ____________________    ________________    _____________________    ________________    ____
 %
-%   VaR_lvl    obs     num_hits        pct_fails        TL           LRuc                  PVuc                 LRcci                 PVcci                  LRcc                  PVcc         
-%   _______    ____    ________    _________________    ___    ________________    ____________________    ________________    ____________________    ________________    _____________________
+%     0.99      1703      166       0.0974750440399295    red    471.596309196609    1.44012903404047e-104    294.473896975476    5.26938462847135e-66    766.070206172084    4.46651045074517e-167    31111.3682479554     0  
 %
-%    0.95      1703      284       0.166764533176747    red    311.998461569185    8.01452391622238e-70    197.952172065323    5.84419941129671e-45    509.950633634508    1.84343099243726e-111
+%
+%    VaR_lvl    obs     num_hits        pct_fails        TL           LRuc                  PVuc                 LRcci                 PVcci                  LRcc                  PVcc                    DQ           PVdq
+%    _______    ____    ________    _________________    ___    ________________    ____________________    ________________    ____________________    ________________    _____________________    ________________    ____
+%
+%     0.95      1703      284       0.166764533176747    red    311.998461569185    8.01452391622238e-70    197.952172065323    5.84419941129671e-45    509.950633634508    1.84343099243726e-111    9638.96137284591     0  
