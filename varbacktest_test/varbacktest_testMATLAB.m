be2269e67f6978bc95264f4329f11d75408c029d
alejandro-cermeno <77359430+alejandro-cermeno@users.noreply.github.com>
2021-11-23 10:25:58 -0500

    Update varbacktest_testMATLAB.m

--- varbacktest_test/varbacktest_testMATLAB.m
+++ varbacktest_test/varbacktest_testMATLAB.m
@@ -1,5 +1,3 @@
-% MathWorks, (2012). Risk Management Toolbox: User's Guide (R2021b).
-
 format long 
 
 % Data collection
@@ -17,28 +15,34 @@ returns = df.mean_true;
 
 % backtest
 for i = 1 : length(VaR_ops)
-    VaR = df.(i + 1);
+    VaR = df.(i + 1); % VaR serie
+
+    % object creation
     vbt = varbacktest(returns, VaR, 'VaRLevel', conf_lvl_ops(i));
-    UC = pof(vbt);
-    obs = UC.Observations;
-    num_hits = UC.Failures;
+    tlObj = tl(vbt);
+    ucObj = pof(vbt);
+    cciObj = cci(vbt);
+    ccObj = cc(vbt);
+
+    % some metrics
+    obs = ucObj.Observations;
+    num_hits = ucObj.Failures;
     pct_fails = num_hits/obs;
-    TL = tl(vbt).TL;
-    LRuc = UC.LRatioPOF;
-    PVuc = UC.PValuePOF;
-    LRcci = cci(vbt).LRatioCCI;
-    PVcci = cci(vbt).PValueCCI;
-    LRcc = cc(vbt).LRatioCC;
-    PVcc = cc(vbt).PValueCC;
+
+    % Engle and Manganelli (2004)
     [DQ, PVdq] = dq(returns, VaR, conf_lvl_ops(i));
     
-    results = table(conf_lvl_ops(i), obs, num_hits, pct_fails, TL, LRuc, PVuc, LRcci, ...
-                    PVcci, LRcc, PVcc, DQ, PVdq, 'VariableNames', {'VaR_lvl', ...
-                    'obs', 'num_hits', 'pct_fails', 'TL', 'LRuc', 'PVuc', 'LRcci', 'PVcci', ...
-                    'LRcc', 'PVcc', 'DQ', 'PVdq'});
+    % results table
+    results = table(conf_lvl_ops(i), obs, num_hits, pct_fails, tlObj.TL,...
+                    ucObj.LRatioPOF, ucObj.PValuePOF, ucObj.POF, ...
+                    cciObj.LRatioCCI, cciObj.PValueCCI, cciObj.CCI, ...
+                    ccObj.LRatioCC, ccObj.PValueCC, ccObj.CC, DQ, ...
+                    PVdq, 'VariableNames', {'VaR_lvl', 'obs', ...
+                    'num_hits', 'pct_fails', 'TL', 'LRuc', 'PVuc', ...
+                    'UC', 'LRcci', 'PVcci', 'CCI', 'LRcc', 'PVcc', ...
+                    'CC', 'DQ', 'PVdq'});
     disp(results)
 end
-
 
 function [DQ, PVdq] = dq(returns, VaR, sigVaR)
 % Dynamic quantile test (DQ) of Engle and Manganelli (2004)
@@ -56,15 +60,12 @@ function [DQ, PVdq] = dq(returns, VaR, sigVaR)
     PVdq    = 1 - chi2cdf(DQ, L+2);
 end
 
-% Output
+%    VaR_lvl    obs     num_hits        pct_fails         TL           LRuc                  PVuc               UC           LRcci                 PVcci             CCI            LRcc                  PVcc               CC             DQ           PVdq
+%    _______    ____    ________    __________________    ___    ________________    _____________________    ______    ________________    ____________________    ______    ________________    _____________________    ______    ________________    ____
 %
-%    VaR_lvl    obs     num_hits        pct_fails         TL           LRuc                  PVuc                  LRcci                 PVcci                  LRcc                  PVcc                    DQ           PVdq
-%    _______    ____    ________    __________________    ___    ________________    _____________________    ________________    ____________________    ________________    _____________________    ________________    ____
+%     0.99      1703      166       0.0974750440399295    red    471.596309196609    1.44012903404047e-104    reject    294.473896975476    5.26938462847135e-66    reject    766.070206172084    4.46651045074517e-167    reject    31111.3682479554     0  
 %
-%     0.99      1703      166       0.0974750440399295    red    471.596309196609    1.44012903404047e-104    294.473896975476    5.26938462847135e-66    766.070206172084    4.46651045074517e-167    31111.3682479554     0  
+%    VaR_lvl    obs     num_hits        pct_fails        TL           LRuc                  PVuc              UC           LRcci                 PVcci             CCI            LRcc                  PVcc               CC             DQ           PVdq
+%    _______    ____    ________    _________________    ___    ________________    ____________________    ______    ________________    ____________________    ______    ________________    _____________________    ______    ________________    ____
 %
-%
-%    VaR_lvl    obs     num_hits        pct_fails        TL           LRuc                  PVuc                 LRcci                 PVcci                  LRcc                  PVcc                    DQ           PVdq
-%    _______    ____    ________    _________________    ___    ________________    ____________________    ________________    ____________________    ________________    _____________________    ________________    ____
-%
-%     0.95      1703      284       0.166764533176747    red    311.998461569185    8.01452391622238e-70    197.952172065323    5.84419941129671e-45    509.950633634508    1.84343099243726e-111    9638.96137284591     0  
+%     0.95      1703      284       0.166764533176747    red    311.998461569185    8.01452391622238e-70    reject    197.952172065323    5.84419941129671e-45    reject    509.950633634508    1.84343099243726e-111    reject    9638.96137284591     0  
